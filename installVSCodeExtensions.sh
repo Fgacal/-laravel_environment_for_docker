8c43d75d3559012c5de5ad212f0b6561b79d2080
IKEASPC\felix.galvan <felix.galvan@bitboxonline.com>
2024-02-13 14:34:14 +0000

    Update installVSCodeExtensions.sh

---
+++ installVSCodeExtensions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension eamodio.gitlens
+code --install-extension ecmel.vscode-html-css
+code --install-extension junstyle.php-cs-fixer
+code --install-extension MehediDracula.php-namespace-resolver
+code --install-extension natqe.reload
+code --install-extension neilbrayfield.php-docblocker
+code --install-extension oderwat.indent-rainbow
+code --install-extension phproberto.vscode-php-getters-setters
+code --install-extension swordev.phpstan
+code --install-extension xdebug.php-debug
+code --install-extension onecentlin.laravel-blade
+code --install-extension codingyu.laravel-goto-view
+code --install-extension ryannaddy.laravel-artisan
+code --install-extension onecentlin.laravel5-snippets
+code --install-extension PKief.material-icon-theme
